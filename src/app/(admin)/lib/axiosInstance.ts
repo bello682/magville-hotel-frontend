c1a8344b0c89660f1c2fd8719119f91565bb9f91
bello682 <belloadetayo14@gmail.com>
2026-08-01 01:40:36 +0100

    Fix useSearchParams Suspense boundaries for Vercel build, new

--- src/app/(admin)/lib/axiosInstance.ts
+++ src/app/(admin)/lib/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL =
   // process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1";
   process.env.NEXT_PUBLIC_API_URL ||
-  "https://magville-hotel-backend.onrender.com";
+  "https://magville-hotel-backend.onrender.com/api/v1";
 
 export const adminAxios = axios.create({
   baseURL: API_BASE_URL,
--- src/store/services/api.ts
+++ src/store/services/api.ts
@@ -4,7 +4,7 @@ export const api = axios.create({
   // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1",
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
-    "https://magville-hotel-backend.onrender.com",
+    "https://magville-hotel-backend.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
