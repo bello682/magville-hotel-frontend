85bf2bbb4bebae7e00563081af63e0b15b3e6843
bello682 <belloadetayo14@gmail.com>
2026-08-03 11:01:26 +0100

    environmental updated

--- src/store/services/api.ts
+++ src/store/services/api.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL:
-    //  process.env.NEXT_PUBLIC_API_URL ||
-    // "https://magville-hotel-backend.onrender.com/api/v1",
-    process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1",
+    process.env.NEXT_PUBLIC_API_URL ||
+    "https://magville-hotel-backend.onrender.com/api/v1",
+  // process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
